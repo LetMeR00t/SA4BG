e762ee5d1cd6518663067d451e39bb96397f98b5
LmR <letmer00t@gmail.com>
2017-09-01 22:01:50 +0200

    Modify python folder name

---
+++ python3/base/equipments/card.py
@@ -0,0 +1,61 @@
+## IMPORTS ##
+
+## CLASS ##
+class Card():
+  """
+  Define a Card class
+  """
+
+  def __init__(self, name="Unknown", value=0):
+    """
+    Default constructor that instanciates a Card class
+    
+    Args:
+        name: A string that represents the name of the new Card
+        value: An integer that represents the value of the new Card
+    
+    Returns:
+        The new object that was instanciated
+    
+    Raises:
+        TypeError : Raise a TypeError if name is not a string or value is not 
+        an integer
+    """
+    if isinstance(name, str):
+      self._name =  name
+    else:
+      raise TypeError('"name" attribute for a Card object must be a string')
+    if isinstance(value, int):
+      self._value =  value
+    else:
+      raise TypeError('"value" attribute for a Card object must be an integer')
+
+  def getName(self):
+    """
+    Get the name of the Card object
+    
+    Args:
+        self: the object
+    
+    Returns:
+        A string that represents the name of this Card
+    
+    Raises:
+        None
+    """
+    return self._name
+
+  def getValue(self):
+    """
+    Get the value of the Card object
+    
+    Args:
+        self: the object
+    
+    Returns:
+        An integer that represents the value of this Card
+    
+    Raises:
+        None
+    """
+    return self._value
---
+++ python3/base/equipments/dice.py
@@ -0,0 +1,44 @@
+## IMPORTS ##
+import random
+
+## CLASS ##
+class Dice():
+  """
+  Define a Dice class
+  """
+
+  def __init__(self, values=['1','2','3','4','5','6']):
+    """
+    Default constructor that instanciates a Dice class
+    
+    Args:
+        values: A list that define each possible value for the new Dice
+    
+    Returns:
+        The new object that was instanciated
+    
+    Raises:
+        TypeError : Raise a TypeError if values is not a list
+    """
+    if isinstance(values, list):
+      self._values =  values
+    else:
+      raise TypeError('"values" attribute for a Dice object must be a list')
+
+  def throwTheDice(self):
+    """
+    Throw the dice and give the result
+    
+    Args:
+        self: the object
+    
+    Returns:
+        A string that represents the result
+    
+    Raises:
+        None
+    """
+    return self._values[random.randint(0,len(self._values)-1)]
+
+x = Dice()
+print(x.throwTheDice())
